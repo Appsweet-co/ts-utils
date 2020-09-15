2c6ac4bcd619f37698ab5596e3c7da9ca76e607d
dperuo <derek@derekperuo.net>
2020-09-15 11:58:32 -0500

    feat: 🎸 () Add missing config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,33 @@
+/* eslint-disable fp/no-mutation */
+/* eslint-disable no-undef */
+
 module.exports = {
-  extends: [
-    "plugin:fp/recommended"
-  ],
+  root: true,
+
+  settings: {
+    "import/resolver": {
+
+      node: {
+        extensions: [".ts", ".tsx"],
+        paths: ["./lib"],
+      },
+
+      alias: [ ]
+    },
+  },
+
+  parser: '@typescript-eslint/parser',
+
   plugins: [
+    '@typescript-eslint',
     "fp",
   ],
+
+  extends: [
+    // 'eslint:recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    "plugin:fp/recommended"
+  ],
+
   rules: { },
 };
