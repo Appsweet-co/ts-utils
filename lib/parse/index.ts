3021a3cdbf676b10b7877b252297f2592d67066e
dperuo <derek@derekperuo.net>
2020-09-16 13:44:13 -0500

    feat: 🎸 (json) Add `stringify()` and `parse()`

---
+++ lib/parse/index.ts
@@ -0,0 +1,19 @@
+import { type } from '../type';
+
+/**
+ * Parse a JSON string, or return the original.
+ *
+ * ```
+ * import { parse } from '@appsweet-co/utils';
+ *
+ * parse('foobar');
+ * // => 'foobar'
+ *
+ * parse('{"hello": "world"}');
+ * // => { hello: 'world' }
+ * ```
+ */
+const parse = (val: any): string =>
+  type('string', val) ? JSON.parse(val) : val;
+
+export { parse };
