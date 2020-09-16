73ceb778fc6d47459b1354ad2966300dcab08e86
dperuo <derek@derekperuo.net>
2020-09-16 11:35:53 -0500

    feat: 🎸 (percent) Add functions

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export * from './lib/curry';
 export * from './lib/divide';
+export * from './lib/percent';
 export * from './lib/pipe';
 export * from './lib/prop';
 export * from './lib/ratio';
---
+++ lib/percent/index.ts
@@ -0,0 +1,39 @@
+import { curry } from '../curry';
+import { ratio } from '../ratio';
+
+/**
+ * Get the numerical percentage of a value compared against `min` and `max` values.
+ *
+ * ```
+ * import { percent } from '@appsweet-co/utils';
+ *
+ * percent(0, 10, 5);
+ * // => 50
+ * ```
+ */
+const percent = (min: number, max: number, val: number) => ratio(min, max, val) * 100;
+
+/**
+ * Same as `percent()`, but curried.
+ *
+ * ```
+ * import { percentC } from '@appsweet-co/utils';
+ *
+ * const scale = percentC(0, 10);
+ *
+ * scale(5);
+ * // => 50
+ * ```
+ *
+ * The following are equivalent:
+ *
+ * ```
+ * percentC(min)(max)(val);
+ * percentC(min)(max, val);
+ * percentC(min, max)(val);
+ * percentC(min, max, val);
+ * ```
+ */
+const percentC: (min: number) => (max: number) => (val: number) => number = curry(percent);
+
+export { percent, percentC };
