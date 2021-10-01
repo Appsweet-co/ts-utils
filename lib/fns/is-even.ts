d71286fd122ecd45273a12dbf4ebaa5390a20f0a
dperuo <derek@derekperuo.net>
2021-10-01 15:34:44 -0500

    feat: (lib/fns) Add isEven() and isOdd()

---
+++ lib/fns/is-even.ts
@@ -0,0 +1,18 @@
+
+/**
+ * Returns true of `x` is an even number.
+ *
+ * @example
+ *
+ * ```ts
+ * isEven(4);
+ * // => true
+ *
+ * isEven(0);
+ * // => true
+ *
+ * isEven(7);
+ * // => false
+ * ```
+ */
+export const isEven = (x: number) => x % 2 === 0;
---
+++ lib/fns/is-odd.ts
@@ -0,0 +1,18 @@
+
+/**
+ * Returns true of `x` is an odd number.
+ *
+ * @example
+ *
+ * ```ts
+ * isOdd(13);
+ * // => true
+ *
+ * isOdd(8);
+ * // => false
+ *
+ * isOdd(0);
+ * // => false
+ * ```
+ */
+export const isOdd = (x: number) => x % 2 !== 0;
