49fea6497a58c30e41f9713074223ca49911d425
dperuo <derek@derekperuo.net>
2021-10-01 09:52:00 -0500

    feat: (lib/fns/stringify.ts) Add file

---
+++ lib/fns/stringify.ts
@@ -0,0 +1,14 @@
+/**
+ * Stringify a value, or return the original if it's already a string.
+ *
+ * @example
+ *
+ * ```ts
+ * stringify(123);
+ * // => '123'
+ *
+ * stringify('foobar');
+ * // => 'foobar'
+ * ```
+ */
+export const stringify = (x: any): string => typeof x === 'string' ? x : JSON.stringify(x);
