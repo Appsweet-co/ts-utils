da9d4cbef61a8574165bebfd0abc04dc3df40183
dperuo <derek@derekperuo.net>
2021-10-06 11:03:18 -0500

    feat: (lib/fns) Add isTruthy() and isFalsy()

---
+++ lib/fns/is-truthy.ts
@@ -0,0 +1,11 @@
+/**
+ * Returns true if `x` is [truthy](https://developer.mozilla.org/en-US/docs/Glossary/Truthy).
+ *
+ * @example
+ *
+ * ```ts
+ * [true, {}, [], 42, "0", "false", new Date(), -42, 12n, 3.14, -3.14, Infinity, -Infinity].every(isTruthy);
+ * // => true
+ * ```
+ */
+export const isTruthy = (x: any) => !!x;
