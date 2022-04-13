7b99cfaa2955cdeaf30baa552b0f0f2c739fed79
dperuo <derek@derekperuo.net>
2022-04-13 12:13:00 -0500

    docs: (arity.ts) Fix typos

--- lib/fns/arity.ts
+++ lib/fns/arity.ts
@@ -14,12 +14,12 @@ export const unary = <T, R>(fn: Nary<T, R>) => (x: T): R => fn(x);
 
 /**
  * Takes a funcation of any [arity](https://en.wikipedia.org/wiki/Arity)
- * and returns a binary function that ignores all but the two parameter.
+ * and returns a binary function that ignores all but the first two parameters.
  */
 export const binary = <T, R>(fn: Nary<T, R>) => (x: T) => (y: T): R => fn(x, y);
 
 /**
  * Takes a funcation of any [arity](https://en.wikipedia.org/wiki/Arity)
- * and returns a binary function that ignores all but the three parameter.
+ * and returns a binary function that ignores all but the first three parameters.
  */
 export const ternary = <T, R>(fn: Nary<T, R>) => (x: T) => (y: T) => (z: T): R => fn(x, y, z);
