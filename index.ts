669fe806e41d43dd8ecad6eedc9ff6361c829ebe
dperuo <derek@derekperuo.net>
2022-04-19 09:49:01 -0500

    feat: (flip.ts) Add file

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ export * from './lib/fns/arity';
 export * from './lib/fns/array';
 export * from './lib/fns/clone';
 export * from './lib/fns/curry';
+export * from './lib/fns/flip';
 export * from './lib/fns/json';
 export * from './lib/fns/logic';
 export * from './lib/fns/math';
---
+++ lib/fns/flip.ts
@@ -0,0 +1,16 @@
+import { Binary } from '../types/functional';
+
+/**
+ * Returns a curried function that accepts arguments in reverse order.
+ *
+ * @example
+ *
+ * ```ts
+ * add(3, 9)
+ * // => 12
+ *
+ * flip(add)(9)(3)
+ * // => 12
+ * ```
+ */
+export const flip = <T, U, R>(fn: Binary<T, U, R>) => (y: U) => (x: T): R => fn(x, y);
