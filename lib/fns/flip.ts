6be352373a68b8dc95a5448cf4c33fcae5918e44
dperuo <derek@derekperuo.net>
2022-04-20 09:16:31 -0500

    feat: (flip.ts) Add flip3()

--- lib/fns/flip.ts
+++ lib/fns/flip.ts
@@ -1,4 +1,4 @@
-import { Binary } from '../types/functional';
+import { Binary, Ternary } from '../types/functional';
 
 /**
  * Returns a curried function that accepts arguments in reverse order.
@@ -14,3 +14,18 @@ import { Binary } from '../types/functional';
  * ```
  */
 export const flip = <T, U, R>(fn: Binary<T, U, R>) => (y: U) => (x: T): R => fn(x, y);
+
+/**
+ * Returns a curried function that accepts arguments in reverse order.
+ *
+ * @example
+ *
+ * ```ts
+ * add(3, 6, 9)
+ * // => 18
+ *
+ * flip(add)(9)(6)(3)
+ * // => 18
+ * ```
+ */
+export const flip3 = <T, U, V, R>(fn: Ternary<T, U, V, R>) => (z: V) => (y: U) => (x: T): R => fn(x, y, z);
