1c82862f5da6f7e92695d7043b02c16bed698a4e
dperuo <derek@derekperuo.net>
2022-04-20 09:57:20 -0500

    docs: (json.ts) Fix whitespace

--- lib/fns/json.ts
+++ lib/fns/json.ts
@@ -1,11 +1,7 @@
-/**
- * Stringify a value, or return the original if it's already a string.
- */
+/** Stringify a value, or return the original if it's already a string. */
 export const stringify = (x: unknown): string => typeof x === 'string' ? x : JSON.stringify(x);
 
-/**
- * Parse a JSON string, or return the original if it fails to parse.
- */
+/** Parse a JSON string, or return the original if it fails to parse. */
 export const parse = (x: string) => {
   try {
     return JSON.parse(x);
