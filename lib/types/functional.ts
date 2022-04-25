2874c864b7253053e71751655c4233e89e32b54a
dperuo <derek@derekperuo.net>
2022-04-25 13:22:26 -0500

    feat: (functional.ts) Add Reducer<T>

--- lib/types/functional.ts
+++ lib/types/functional.ts
@@ -3,3 +3,15 @@ export type Curry2<T, U, R> = (x: T) => (y: U) => R;
 
 /** A curried function that takes three arguments. */
 export type Curry3<T, U, V, R> = (x: T) => (y: U) => (z: V) => R;
+
+/**
+ * A function called once on each item in an array and returns the accumulated
+ * result. The function passes the current accumulated value as the first argument
+ * of the next function call.
+ *
+ * @param acc The accumulated result.
+ * @param next The next item in the array.
+ * @param index The array index of the next item.
+ * @param list The original array.
+ */
+export type Reducer<T> = (acc: T, next: T, index?: number, list?: T[]) => T;
