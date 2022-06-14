6d24374783a82a842080065db8b95e6827a527d5
Derek Peruo <derek@appsweet.co>
2022-06-14 14:54:55 -0500

    feat: (iso.ts) Add file

---
+++ lib/types/iso.ts
@@ -0,0 +1,11 @@
+/** A string formatted as [ISO 3166-1](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2).*/
+declare type ISOCountry = Nominal<'ISOCountry', string>;
+
+/** A string formatted as [ISO 639-1](https://en.wikipedia.org/wiki/ISO_639-1).*/
+declare type ISOLanguage = Nominal<'ISOLanguage', string>;
+
+/** A string formatted as `xx-YY`, where `xx` is an {@linkcode ISOLanguage} and `YY` is an {@linkcode ISOCountry}. */
+declare type ISOLocale = Nominal<'ISOLocale', string>;
+
+/** A string formatted as [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601).*/
+declare type ISOTimestamp = Nominal<'ISOTimestamp', string>;
