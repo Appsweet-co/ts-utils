e7891e7bedcbaa2b8bb09b0fd0466369aff49dc8
dperuo <derek@derekperuo.net>
2022-06-16 08:24:52 -0500

    fix: (timestamp.ts) Delete duplicate type

--- lib/types/timestamp.ts
+++ lib/types/timestamp.ts
@@ -1,8 +1,5 @@
 import type { Nominal } from './nominal';
 
-/** A string formatted as [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601).*/
-export type ISOString = Nominal<'ISOString', string>;
-
 /** A [Unix timestamp](https://en.wikipedia.org/wiki/Unix_time) measured in milliseconds. */
 export type UnixMillis = Nominal<'UnixMillis', number>;
 
