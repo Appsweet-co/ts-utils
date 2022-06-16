95bfd1c7f6fd1e4fabaad200787744ae7bbd376d
dperuo <derek@derekperuo.net>
2022-06-16 08:21:37 -0500

    fix: (never.ts) Better doc block and export

--- lib/fns/never.ts
+++ lib/fns/never.ts
@@ -1,22 +1,12 @@
 /**
- * A function that takes an argument of type `never`. Use this function for 
+ * A function that never takes or returns a value. Useful for things like
  * [Exhaustiveness Checking](https://www.typescriptlang.org/docs/handbook/2/narrowing.html#exhaustiveness-checking).
- * 
- * Throws a `TypeError` when executed.
  *
  * @example
  *
  * ```ts
- * function select(x: 3 | 6 | 9) {
- *   switch(x) {
- *     case 3: return 'three';
- *     case 6: return 'six';
- *     case 9: return 'nine';
- *     default: return never(x);
- *   }
- * }
+ * never('anything')
+ * // => never
  * ```
  */
-const never = (x: never) => {
-   throw new TypeError(`The never() function expects a never type, instead got ${typeof x}`);
-}
+export const never = (x: never) => x;
