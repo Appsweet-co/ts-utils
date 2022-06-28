72d22c30e4165249ca65a1730ee3b643bacb48eb
dperuo <derek@derekperuo.net>
2022-06-28 08:55:46 -0500

    feat: (json.ts) Add parseOr()

--- lib/fns/json.ts
+++ lib/fns/json.ts
@@ -3,5 +3,36 @@ import { throwable } from './adt';
 /** Stringify a value, or return the original if it's already a string. */
 export const stringify = (x: unknown): string => typeof x === 'string' ? x : JSON.stringify(x);
 
-/** Parse a JSON string, or return the original if it fails to parse. */
+/**
+ * Parse a JSON string, or return the original if it fails to parse.
+ *
+ * @see {@linkcode parseOr}
+ *
+ * @example
+ *
+ * ```ts
+ * parse('["Good","JSON"]')
+ * // => ['Good', 'JSON']
+ *
+ * parse('[Bad, JSON]')
+ * // => '[Bad, JSON]'
+ * ```
+ */
 export const parse = <R = unknown>(x: string): string | R => throwable(JSON.parse)(x)(x) as R;
+
+/**
+ * Parse a JSON string, or return the fallback if it fails to parse.
+ *
+ * @see {@linkcode parse}
+ *
+ * @example
+ *
+ * ```ts
+ * parseOr(['fallback'])('["Good","JSON"]')
+ * // => ['Good', 'JSON']
+ *
+ * parseOr(['fallback'])('[Bad, JSON]')
+ * // => ['fallback']
+ * ```
+ */
+export const parseOr = <R>(fallback: R) => (x: string): R => throwable(JSON.parse)(fallback)(x) as R;
