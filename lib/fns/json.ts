6718523764ad7e57c7e12f43a1e20e42b2490726
dperuo <derek@derekperuo.net>
2022-06-28 08:44:37 -0500

    refactor: (json.ts) Use throwable()

--- lib/fns/json.ts
+++ lib/fns/json.ts
@@ -1,12 +1,7 @@
+import { throwable } from './adt';
+
 /** Stringify a value, or return the original if it's already a string. */
 export const stringify = (x: unknown): string => typeof x === 'string' ? x : JSON.stringify(x);
 
 /** Parse a JSON string, or return the original if it fails to parse. */
-export const parse = <R = unknown>(x: string): string | R => {
-  // eslint-disable-next-line functional/no-try-statement
-  try {
-    return JSON.parse(x) as R;
-  } catch (_) {
-    return x;
-  }
-};
+export const parse = <R = unknown>(x: string): string | R => throwable(JSON.parse)(x)(x) as R;
