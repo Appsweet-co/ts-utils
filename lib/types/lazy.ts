cb3eec82c4332b15e6224c0266b1689772753682
dperuo <derek@appsweet.co>
2022-07-18 16:17:09 -0500

    feat: (lazy.ts) Delete file

--- lib/types/lazy.ts
+++
@@ -1,9 +0,0 @@
-import { Nullary } from './arity';
-
-/**
- * Syntatic sugar for {@linkcode Nullary}. Useful for things like
- * [lazy evaluation](https://en.wikipedia.org/wiki/Lazy_evaluation).
- *
- * @alias {@linkcode Nullary}
- */
-export type Lazy<T> = Nullary<T>;
