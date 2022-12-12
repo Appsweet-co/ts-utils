fc401c508ed3c24fcf5e220347f5170b3f2955af
dperuo <derek@appsweet.co>
2022-12-11 19:45:33 -0600

    feat: (adt.ts) Deprecate functions

--- lib/fns/adt.ts
+++ lib/fns/adt.ts
@@ -1,75 +1,26 @@
-import type { Nary, Nullary, Unary } from '../types/arity';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import type { Nullary, Unary } from '../types/arity';
 import type { Predicate } from '../types/predicate';
 import { isNil } from './predicate';
 
+import type { Either } from '../types/either';
+import type { Option } from '../types/option';
+import type { Result } from '../types/result';
+
 /**
- * Returns the result of `right(x)` if the predicate is true,
- * or `left(x)` if the predicate is false.
- *
- * @example
- *
- * ```ts
- * either(add(2))(sub(2))(isEven)(42)
- * // => 44
- *
- * either(add(2))(sub(2))(isEven)(43)
- * // => 41
- * ```
+ * @deprecated In v0.35.0. Use {@linkcode Either} instead.
  */
 export const either = <T, R>(right: Unary<T, R>) => (left: Unary<T, R>) => (pred: Predicate<T>) => (x: T): R => {
   return pred(x) ? right(x) : left(x);
 };
 
 /**
- * Returns the result of `just(x)`, or the fallback if `x` is null or undefined.
- *
- * @example
- *
- * ```ts
- * maybe(99)(add(2))(42)
- * // => 44
- *
- * maybe(99)(add(2))(undefined)
- * // => 99
- * ```
+ * @deprecated In v0.35.0. Use {@linkcode Option} instead.
  */
 export const maybe = <R>(fallback: R) => <T>(just: Unary<T, R>) => (x: T): R => isNil(x) ? fallback : just(x);
 
 /**
- * Returns the result of `ok(x)`, or the result of `err()`
- * if `x` is null or undefined.
- *
- * @example
- *
- * ```ts
- * result(add(2))(() => 99)(42)
- * // => 44
- *
- * result(add(2))(() => 99)(null)
- * // => 99
- * ```
+ * @deprecated In v0.35.0. Use {@linkcode Result} instead.
  */
 export const result = <T, R>(ok: Unary<T, R>) => (err: Nullary<R>) => (x: T): R => isNil(x) ? err() : ok(x);
-
-/**
- * Wraps `fn` in a [`try...catch`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch) block
- * and returns the result of `fn(x)`, or returns the fallback if `fn(x)` throws an error.
- *
- * @example
- *
- * ```ts
- * throwable(JSON.parse)('fallback')('["Good","JSON"]')
- * // => ['Good', 'JSON']
- *
- * throwable(JSON.parse)('fallback')('[Bad, JSON]')
- * // => 'fallback'
- * ```
- */
-export const throwable = <T, R>(fn: Nary<T, R>) => (fallback: R) => (x: T): R => {
-  // eslint-disable-next-line functional/no-try-statement
-  try {
-    return fn(x);
-  } catch (_) {
-    return fallback;
-  }
-};
