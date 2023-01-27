9a8c90107223135a07d73c7b742a205e57371bad
dperuo <derek@appsweet.co>
2023-01-27 10:00:55 -0600

    feat: (array.ts) Add Head<T> and Tail<T>
    Hat Tip: https://rxjs.dev/api?type=type-alias

--- lib/types/array.ts
+++ lib/types/array.ts
@@ -9,3 +9,27 @@
  * ```
  */
 export type ValuesInside<T extends readonly unknown[]> = T extends ReadonlyArray<infer U> ? U : never;
+
+/**
+ * Extracts the type info for the first item inside an array.
+ *
+ * @example
+ *
+ * ```ts
+ * type Value = Head<[true, 1, '2']>;
+ * // => true
+ * ```
+ */
+export type Head<T extends readonly unknown[]> = T[0];
+
+/**
+ * Extracts the type info for all but the first item inside an array.
+ *
+ * @example
+ *
+ * ```ts
+ * type Value = Tail<[true, 1, '2']>;
+ * // => [1 | "2"]
+ * ```
+ */
+export type Tail<T extends readonly unknown[]> = ((...xs: T) => unknown) extends ((head: infer _, ...rest: infer U) => unknown) ? U : never;
