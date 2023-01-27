5368bd2e36e2bb63855b9c92c6fb2da6ee053864
dperuo <derek@appsweet.co>
2023-01-27 12:39:31 -0600

    docs: (array.ts) Better doc blocks

--- lib/types/array.ts
+++ lib/types/array.ts
@@ -11,7 +11,7 @@
 export type ValuesInside<T extends readonly unknown[]> = T extends ReadonlyArray<infer U> ? U : never;
 
 /**
- * Extracts the type info for the first item inside an array.
+ * Extracts the type info for the first item inside a tuple.
  *
  * @example
  *
@@ -23,13 +23,13 @@ export type ValuesInside<T extends readonly unknown[]> = T extends ReadonlyArray
 export type Head<T extends readonly unknown[]> = T[0];
 
 /**
- * Extracts the type info for all but the first item inside an array.
+ * Extracts the type info for all but the first item inside a tuple.
  *
  * @example
  *
  * ```ts
  * type Value = Tail<[true, 1, '2']>;
- * // => [1 | "2"]
+ * // => [1, "2"]
  * ```
  */
 export type Tail<T extends readonly unknown[]> = ((...xs: T) => unknown) extends ((head: infer _, ...rest: infer U) => unknown) ? U : never;
