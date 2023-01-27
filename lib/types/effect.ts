bb39558b96c3e3f193f5c5119df16ffb8806948f
dperuo <derek@appsweet.co>
2023-01-27 12:13:04 -0600

    docs: (effect.ts) Add example code

--- lib/types/effect.ts
+++ lib/types/effect.ts
@@ -3,5 +3,11 @@ import { Nominal } from './nominal';
 /**
  * A type that generates [side effects](https://en.wikipedia.org/wiki/Side_effect_(computer_science))
  * as part of its regular execution.
+ *
+ * @example
+ *
+ * ```ts
+ * type Foo = (x: number) => Effect<boolean>;
+ * ```
  */
 export type Effect<T> = Nominal<'Effect', T>;
