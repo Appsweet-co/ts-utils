c0caa0f032bdfb4049c8b1504f147c69bfdc8d7a
Derek Peruo <derek@appsweet.co>
2024-08-21 10:30:33 -0500

    feat: (string.ts) Add chunk()

--- lib/fns/string.ts
+++ lib/fns/string.ts
@@ -6,3 +6,6 @@ export const uppercase = (x: string) => x.toUpperCase();
 
 /** Removes all whitespace and line terminators around a string. Same as `x.trim()`. */
 export const trim = (x: string) => x.trim();
+
+/** Returns `text` as a string broken into chunks of `size`, separated by spaces. */
+export const chunk = (size: number) => (text: string): string => (text.match(new RegExp(`.{1,${size}}`, 'g')) ?? []).join(' ');
